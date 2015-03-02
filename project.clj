759235723d1c0e2743055d5eea1a4d3a87a24cfa
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-01 18:05:10 -0800

    release v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject precursor/om-i "0.1.0-SNAPSHOT"
+(defproject precursor/om-i "0.1.0"
   :description "Instrumentation helpers for Om applications"
   :url "https://github.com/PrecursorApp/om-i"
 
