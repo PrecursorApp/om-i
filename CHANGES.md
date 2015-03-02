fcbdff8f2a59426a24ea2e990b8a9cf9b613bf58
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-02 15:54:01 -0800

    release 0.1.3, with no cljs-time dependency

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+## 0.1.3
+### Changes
+* remove unused cljs-time requires
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject precursor/om-i "0.1.2"
+(defproject precursor/om-i "0.1.3"
   :description "Instrumentation helpers for Om applications"
   :url "https://github.com/PrecursorApp/om-i"
   :license {:name "Eclipse Public License"
