06bdf0f91f026140d0ad623804e114bf0f8ff3e7
Daniel Woelfel <dwwoelfel@gmail.com>
2015-03-02 10:15:51 -0800

    add license to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject precursor/om-i "0.1.1"
   :description "Instrumentation helpers for Om applications"
   :url "https://github.com/PrecursorApp/om-i"
-
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2755" :scope "provided"]
                  [org.omcljs/om "0.8.8" :scope "provided"]]
